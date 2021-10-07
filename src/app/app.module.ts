c4ff927b0df1cc1350ec0b5ed8ffa352e2506d96
sathya1508 <sathya1508@gmail.com>
2021-10-07 23:24:12 +0530

    InitialCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoansComponent } from './loans/loans.component';
 import { AddloansComponent } from './addloans/addloans.component';
 
+const routes: Routes = [
+  { path: 'loans', component: LoansComponent },
+  { path: 'loans/add', component: AddloansComponent },
+];
+
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
   declarations: [AppComponent, LoansComponent, AddloansComponent],
   bootstrap: [AppComponent],
+  exports: [RouterModule],
 })
 export class AppModule {}
